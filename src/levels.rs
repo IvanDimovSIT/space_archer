419497d29c158160317321dcd438587413e1abdb
IvanDimovSIT <ivan390@abv.bg>
2026-05-09 12:24:37 +0300

    Added 2 new levels

--- src/levels.rs
+++ src/levels.rs
@@ -15,6 +15,8 @@ pub fn create_levels() -> Vec<LevelTemplate> {
         half_strength_level(),
         moving_2_planets_level(),
         ufo_introduction_level(),
+        ufo_launch_down_with_planet_level(),
+        three_planets_moving_brarier_level()
     ]
 }
 
@@ -125,6 +127,40 @@ fn ufo_introduction_level() -> LevelTemplate {
         target,
         ufos: vec![ufo],
         bariers: vec![barrier],
+        ..Default::default()
+    }
+}
+
+fn ufo_launch_down_with_planet_level() -> LevelTemplate {
+    let target = TargetTemplate::new_static(TargetFlip::Bottom, vec2(150.0, 35.0));
+    let ufo = UFOTemplate::new_static(
+        vec2(18.0, 40.0),
+        vec2(110.0, -50.0),
+    );
+    let barrier = BarierTemplate::new_static(Rect::new(80.0, -10.0, 10.0, 60.0));
+    let planet = PlanetTemplate::new_static(19.0, vec2(115.0, 30.0), PlanetAppearance::Brown);
+
+    LevelTemplate {
+        target,
+        ufos: vec![ufo],
+        bariers: vec![barrier],
+        planets: vec![planet],
+        ..Default::default()
+    }
+}
+
+fn three_planets_moving_brarier_level() -> LevelTemplate {
+    let target = TargetTemplate::new_static(TargetFlip::Right, vec2(115.0, 48.0));
+    let barrier = BarierTemplate::new_static(Rect::new(60.0, 30.0, 10.0, 40.0));
+    let ufo = UFOTemplate::new_static(vec2(30.0, 65.0), vec2(75.0, -65.0));
+    let planet1 = PlanetTemplate::new_static(14.0, vec2(50.0, -25.0), PlanetAppearance::Brown);
+    let planet2 = PlanetTemplate::new_static(22.0, vec2(125.0, 15.0), PlanetAppearance::Red);
+
+    LevelTemplate {
+        target,
+        bariers: vec![barrier],
+        planets: vec![planet1, planet2],
+        ufos: vec![ufo],
         ..Default::default()
     }
 }
